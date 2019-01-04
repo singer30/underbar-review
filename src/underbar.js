9754cc253f3ff0fc01ac7292db3338c25b82e3bf
singer30 <lukas.w.singer@gmail.com>
2019-01-03 16:15:17 -0800

    implemented map and pluck.

--- src/underbar.js
+++ src/underbar.js
@@ -106,14 +106,17 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    
   };
 
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+    var mappedArray = [];
+    _.each(collection, function(value) { 
+      mappedArray.push(iterator(value));   
+    });
+    return mappedArray;
   };
 
   /*
